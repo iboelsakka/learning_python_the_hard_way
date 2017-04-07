520460617253ea219c7805f79b06db4448af52ca
Ibrahim Elsakk <ibrahim.elsakka@gmail.com>
2017-04-07 14:44:54 -0700

    Updating ex5.py

--- ex5.py
+++ ex5.py
@@ -1,17 +1,16 @@
-my_name = "Ibrahim Elsakka"
-my_age = 20
-my_height = 70
-my_weight = 205
-my_eyes = "Brown"
-my_teeth = "White"
-my_hair = "Brown"
+name = "Ibrahim Elsakka"
+age = 20
+height = 70
+weight = 205
+eyes = "Brown"
+teeth = "White"
+hair = "Brown"
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." %my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." %teeth
 
-print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
-#print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." %(age, height, weight, age + height + weight)
