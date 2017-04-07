a565934314bbd7f5a2df7e68b203a70395c8e6b2
Ibrahim Elsakk <ibrahim.elsakka@gmail.com>
2017-04-07 14:28:34 -0700

    initial checkin

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6 # 25 + 5 = 5
+print "Roosers", 100 - 25 * 3 % 4 # 100 - (75) % 4 = 100 - (3) = 97
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # 3 + 2 + 1 - 5 + 0 - 0 + 6 = 7
+
+print "Is it true that 3 + 2 < 5 - 7" # 5 < -2 = false
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+
+print "Is it greater or equal?", 5 >= 2
+print "Is it less or equal?", 5 <= -2
