5df6721c9099f7f5c77cc6ec42bb07be51783dc2
Ibrahim Elsakk <ibrahim.elsakka@gmail.com>
2017-04-11 15:56:49 -0700

    Work

--- ex17.py
+++ ex17.py
@@ -5,22 +5,26 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
+# print "Copying from %s to %s" % (from_file, to_file)
 
-# we could combine these two lines by ???
-in_file = open(from_file)
-indata = in_file.read()
+# we could combine these two lines by adding .read() at the end
+#in_file = open(from_file).read()
+# indata = in_file.read()
 
-print "The input file is %d bytes long" % len(indata)
+# print "The input file is %d bytes long" % len(indata)
 
-print "Does the output file exist? %r" %exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
+# print "Does the output file exist? %r" %exists(to_file)
+# print "Ready, hit RETURN to continue, CTRL-C to abort."
+# raw_input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+#out_file = open(to_file, 'w').write(in_file)
+# out_file.write(indata)
 
-print "Alright, all done."
+# With this single line of code, we read the source file and write it to the 
+	# destination file.
+out_file = open(to_file, 'w').write(open(from_file).read())
 
-out_file.close()
-in_file.close()
+# print "Alright, all done."
+
+#out_file.close()
+#in_file.close()
---
+++ ex19.py
@@ -0,0 +1,26 @@
+# Created by Ibrahim Elsakka on 4/11/2017
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+	print "You have %d cheeses!" % cheese_count
+	print "You have %d boxes of crackers!" % boxes_of_crackers
+	print "Man that's enough for a party!"
+	print "Get a blanket.\n"
+
+print "We can just give the function numbers directly:"
+# calls the cheese_and_crackers function passing in the values 20 and 30
+cheese_and_crackers(20, 30)
+
+print "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+# calls the cheese_and_crackers functin passing in 10 and 50
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print "We can even do math inside too:"
+# calls the cheese_and_crackers function passing in 30 and 11
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print "And we can combine the two, variables and math:"
+# calls the cheese_and_crackers function passing in 110 and 1050
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
